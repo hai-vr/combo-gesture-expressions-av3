da018840d802523f9fd5df5256da4f999f0d6ac0
Haï~ <hai-vr@users.noreply.github.com>
2020-08-19 18:51:34 +0200

    TD: Remove unused equality comparer

--- Assets/Hai/ComboGesture/Scripts/Editor/Internal/ComboValue.cs
+++ Assets/Hai/ComboGesture/Scripts/Editor/Internal/ComboValue.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-
-namespace Hai.ComboGesture.Scripts.Editor.Internal
+﻿namespace Hai.ComboGesture.Scripts.Editor.Internal
 {
     internal class ComboValue
     {
@@ -15,23 +13,6 @@ namespace Hai.ComboGesture.Scripts.Editor.Internal
             Left = rawValue / 10;
             Right = rawValue % 10;
             IsSymmetrical = Left == Right;
-        }
-
-        private sealed class ComboEqualityComparer : IEqualityComparer<ComboValue>
-        {
-            public bool Equals(ComboValue x, ComboValue y)
-            {
-                if (ReferenceEquals(x, y)) return true;
-                if (ReferenceEquals(x, null)) return false;
-                if (ReferenceEquals(y, null)) return false;
-                if (x.GetType() != y.GetType()) return false;
-                return x.RawValue == y.RawValue;
-            }
-
-            public int GetHashCode(ComboValue obj)
-            {
-                return obj.RawValue;
-            }
         }
     }
 }
