b267a4f7fb0aedd82f71024e61b5c667e9acd98e
Haï~ <hai-vr@users.noreply.github.com>
2020-08-21 19:59:39 +0200

    Add buttons to auto-set animations

--- Assets/Hai/ComboGesture/Scripts/Editor/EditorUI/CGEActivityEditorWindow.cs
+++ Assets/Hai/ComboGesture/Scripts/Editor/EditorUI/CGEActivityEditorWindow.cs
@@ -22,74 +22,8 @@ namespace Hai.ComboGesture.Scripts.Editor.EditorUI
         private const int PictureWidth = 120;
         private const int PictureHeight = 80;
         private const int GuiSquareWidth = 140;
+
         private static readonly int GuiSquareHeight = (int) (singleLineHeight * 3 + PictureHeight);
-
-        private static readonly Dictionary<string, string> Equivs = new Dictionary<string, string>
-        {
-            {"anim00", "No gesture"},
-            {"anim01", "FIST"},
-            {"anim02", "OPEN"},
-            {"anim03", "POINT"},
-            {"anim04", "PEACE"},
-            {"anim05", "ROCKNROLL"},
-            {"anim06", "GUN"},
-            {"anim07", "THUMBSUP"},
-            {"anim11", "FIST x2, L+R trigger"},
-            {"anim11_L", "FIST x2, LEFT trigger"},
-            {"anim11_R", "FIST x2, RIGHT trigger"},
-            {"anim12", "OPEN + FIST"},
-            {"anim13", "POINT + FIST"},
-            {"anim14", "PEACE + FIST"},
-            {"anim15", "ROCKNROLL + FIST"},
-            {"anim16", "GUN + FIST"},
-            {"anim17", "THUMBSUP + FIST"},
-            {"anim22", "OPEN x2"},
-            {"anim23", "OPEN + POINT"},
-            {"anim24", "OPEN + PEACE"},
-            {"anim25", "OPEN + ROCKNROLL"},
-            {"anim26", "OPEN + GUN"},
-            {"anim27", "OPEN + THUMBSUP"},
-            {"anim33", "POINT x2"},
-            {"anim34", "POINT + PEACE"},
-            {"anim35", "POINT + ROCKNROLL"},
-            {"anim36", "POINT + GUN"},
-            {"anim37", "POINT + THUMBSUP"},
-            {"anim44", "PEACE x2"},
-            {"anim45", "PEACE + ROCKNROLL"},
-            {"anim46", "PEACE + GUN"},
-            {"anim47", "PEACE + THUMBSUP"},
-            {"anim55", "ROCKNROLL x2"},
-            {"anim56", "ROCKNROLL + GUN"},
-            {"anim57", "ROCKNROLL + THUMBSUP"},
-            {"anim66", "GUN x2"},
-            {"anim67", "GUN + THUMBSUP"},
-            {"anim77", "THUMBSUP x2"}
-        };
-
-        private static readonly Dictionary<string, MergePair> ParameterToMerge = new Dictionary<string, MergePair>
-        {
-            {"anim12", new MergePair("anim01", "anim02")},
-            {"anim13", new MergePair("anim01", "anim03")},
-            {"anim14", new MergePair("anim01", "anim04")},
-            {"anim15", new MergePair("anim01", "anim05")},
-            {"anim16", new MergePair("anim01", "anim06")},
-            {"anim17", new MergePair("anim01", "anim07")},
-            {"anim23", new MergePair("anim02", "anim03")},
-            {"anim24", new MergePair("anim02", "anim04")},
-            {"anim25", new MergePair("anim02", "anim05")},
-            {"anim26", new MergePair("anim02", "anim06")},
-            {"anim27", new MergePair("anim02", "anim07")},
-            {"anim34", new MergePair("anim03", "anim04")},
-            {"anim35", new MergePair("anim03", "anim05")},
-            {"anim36", new MergePair("anim03", "anim06")},
-            {"anim37", new MergePair("anim03", "anim07")},
-            {"anim45", new MergePair("anim04", "anim05")},
-            {"anim46", new MergePair("anim04", "anim06")},
-            {"anim47", new MergePair("anim04", "anim07")},
-            {"anim56", new MergePair("anim05", "anim06")},
-            {"anim57", new MergePair("anim05", "anim07")},
-            {"anim67", new MergePair("anim06", "anim07")}
-        };
 
         public AnimationClip noAnimationClipNullObject;
         private Dictionary<AnimationClip, Texture2D> _animationClipToTextureDict;
@@ -106,17 +40,28 @@ namespace Hai.ComboGesture.Scripts.Editor.EditorUI
         private bool _firstTimeSetup;
         private AutoSetupPreview.SetupResult? _setupResult;
 
+        private CgeActivityEditorDriver _driver;
+
         private SerializedProperty _mergeTarget;
         private SerializedProperty _mergeLeft;
         private SerializedProperty _mergeRight;
+
+        private static GUIStyle _middleAligned;
+        private static GUIStyle _middleAlignedBold;
+        private static GUIStyle _largeFont;
 
         private void OnEnable()
         {
             noAnimationClipNullObject = new AnimationClip();
             _animationClipToTextureDict = new Dictionary<AnimationClip, Texture2D>();
+            _driver = new CgeActivityEditorDriver();
+
+            _middleAligned = new GUIStyle {alignment = TextAnchor.MiddleCenter, clipping = TextClipping.Overflow};
+            _middleAlignedBold = new GUIStyle(EditorStyles.boldLabel) {alignment = TextAnchor.MiddleCenter, clipping = TextClipping.Overflow};
+            _largeFont = new GUIStyle {fontSize = 20};
         }
 
-        private void OnGUI()
+        private void OnInspectorUpdate()
         {
             var activeGameObject = Selection.activeGameObject;
             if (activeGameObject != null)
@@ -126,9 +71,13 @@ namespace Hai.ComboGesture.Scripts.Editor.EditorUI
                 {
                     activity = selectedActivity;
                     serializedObject = null;
+                    Repaint();
                 }
             }
+        }
 
+        private void OnGUI()
+        {
             if (activity != null && serializedObject == null)
             {
                 serializedObject = new SerializedObject(activity);
@@ -404,9 +353,7 @@ namespace Hai.ComboGesture.Scripts.Editor.EditorUI
 
         private void LayoutPreventEyesBlinking()
         {
-            GUIStyle myStyle = new GUIStyle();
-            myStyle.fontSize = 20;
-            GUILayout.Label("Select face expressions with <b>both eyes closed</b>.", myStyle);
+            GUILayout.Label("Select face expressions with <b>both eyes closed</b>.", _largeFont);
             GUILayout.BeginArea(new Rect(0, singleLineHeight * 3, position.width, GuiSquareHeight * 8));
             var allClips = new HashSet<AnimationClip>(activity.OrderedAnimations().Where(clip => clip != null)).ToList();
             var mod = Math.Max(3, Math.Min(8, (int)Math.Sqrt(allClips.Count)));
@@ -432,44 +379,71 @@ namespace Hai.ComboGesture.Scripts.Editor.EditorUI
         private void DrawInner(string propertyPath)
         {
             var property = serializedObject.FindProperty(propertyPath);
-            if (Equivs[propertyPath].Contains("x2"))
-            {
-                var guiStyle = new GUIStyle(EditorStyles.boldLabel);
-                guiStyle.alignment = TextAnchor.MiddleCenter;
-                guiStyle.clipping = TextClipping.Overflow;
-                GUILayout.Label(Equivs[propertyPath], guiStyle);
-            }
-            else
-            {
-                var guiStyle = new GUIStyle();
-                guiStyle.alignment = TextAnchor.MiddleCenter;
-                guiStyle.clipping = TextClipping.Overflow;
-                GUILayout.Label(Equivs[propertyPath], guiStyle);
-            }
+            GUILayout.Label(_driver.ShortTranslation(propertyPath), _driver.IsSymmetrical(propertyPath) ? _middleAlignedBold : _middleAligned);
 
             GUILayout.BeginArea(new Rect((GuiSquareWidth - PictureWidth) / 2, singleLineHeight, PictureWidth, PictureHeight));
             var element = property.objectReferenceValue != null ? (AnimationClip) property.objectReferenceValue : null;
             DrawPreviewOrRefreshButton(element);
             GUILayout.EndArea();
 
-            if (ParameterToMerge.ContainsKey(propertyPath)) {
-                var merge = ParameterToMerge[propertyPath];
-                var left = serializedObject.FindProperty(merge.Left).objectReferenceValue;
-                var right = serializedObject.FindProperty(merge.Right).objectReferenceValue;
-                var areMergeAnimationsDefinedAndNotTheSame = left != null && right != null && left != right;
+            if (_driver.IsAPropertyThatCanBeCombined(propertyPath))
+            {
+                var areSourcesCompatible = _driver.AreCombinationSourcesDefinedAndCompatible(serializedObject, propertyPath);
 
-                EditorGUI.BeginDisabledGroup(!areMergeAnimationsDefinedAndNotTheSame);
-                GUILayout.BeginArea(new Rect(GuiSquareWidth - 2 * singleLineHeight, PictureHeight - singleLineHeight * 0.5f, singleLineHeight * 2, singleLineHeight * 1.5f));
-                if (GUILayout.Button("M"))
+                var rect = element != null
+                    ? new Rect(GuiSquareWidth - 2 * singleLineHeight, PictureHeight - singleLineHeight * 0.5f, singleLineHeight * 2, singleLineHeight * 1.5f)
+                    : new Rect(GuiSquareWidth - 100, PictureHeight - singleLineHeight * 0.5f, 100, singleLineHeight * 1.5f);
+
+                EditorGUI.BeginDisabledGroup(!areSourcesCompatible);
+                GUILayout.BeginArea(rect);
+                if (GUILayout.Button(element != null ? "+": "+ Combine"))
                 {
+                    var merge = _driver.ProvideCombinationPropertySources(propertyPath);
                     OpenMergeWindowFor(propertyPath, merge.Left, merge.Right);
                 }
                 GUILayout.EndArea();
                 EditorGUI.EndDisabledGroup();
             }
+            else
+            {
+                BeginInvisibleRankPreservingArea();
+                InvisibleRankPreservingButton();
+                EndInvisibleRankPreservingArea();
+            }
+
+            if (element == null && _driver.IsAutoSettable(propertyPath))
+            {
+                var propertyPathToCopyFrom = _driver.GetAutoSettableSource(propertyPath);
+                var animationToBeCopied = serializedObject.FindProperty(propertyPathToCopyFrom).objectReferenceValue;
+
+                EditorGUI.BeginDisabledGroup(animationToBeCopied == null);
+                GUILayout.BeginArea(new Rect(GuiSquareWidth - 100, PictureHeight - singleLineHeight * 1.75f, 100, singleLineHeight * 1.5f));
+                if (GUILayout.Button("Auto-set"))
+                {
+                    AutoSet(propertyPath, propertyPathToCopyFrom);
+                }
+                GUILayout.EndArea();
+                EditorGUI.EndDisabledGroup();
+            }
+            else
+            {
+                BeginInvisibleRankPreservingArea();
+                InvisibleRankPreservingButton();
+                EndInvisibleRankPreservingArea();
+            }
 
             GUILayout.Space(PictureHeight);
             EditorGUILayout.PropertyField(property, GUIContent.none);
+        }
+
+        private static void BeginInvisibleRankPreservingArea()
+        {
+            GUILayout.BeginArea(new Rect(-1000, -1000, 0, 0));
+        }
+
+        private static void EndInvisibleRankPreservingArea()
+        {
+            GUILayout.EndArea();
         }
 
         private void OpenMergeWindowFor(string target, string left, string right)
@@ -478,6 +452,12 @@ namespace Hai.ComboGesture.Scripts.Editor.EditorUI
             _mergeLeft = serializedObject.FindProperty(left);
             _mergeRight = serializedObject.FindProperty(right);
             _editorMode = EditorMode.CombineFaceExpressions;
+        }
+
+        private void AutoSet(string propertyPath, string propertyPathToCopyFrom)
+        {
+            serializedObject.FindProperty(propertyPath).objectReferenceValue = serializedObject.FindProperty(propertyPathToCopyFrom).objectReferenceValue;
+            serializedObject.ApplyModifiedProperties();
         }
 
         private void DrawBlinkingSwitch(AnimationClip element)
@@ -531,46 +511,52 @@ namespace Hai.ComboGesture.Scripts.Editor.EditorUI
 
         private void DrawPreviewOrRefreshButton(AnimationClip element)
         {
-            if (element == null) return;
+            if (element != null) {
+                var clipIsInDict = _animationClipToTextureDict.ContainsKey(element);
 
-            var clipIsInDict = _animationClipToTextureDict.ContainsKey(element);
-            if (clipIsInDict)
-            {
-                GUILayout.Box(_animationClipToTextureDict[element], GUILayout.ExpandHeight(true), GUILayout.ExpandWidth(true));
-            }
-
-            EditorGUILayout.BeginFadeGroup(!clipIsInDict ? 1 : 0);
-            EditorGUI.BeginDisabledGroup(AnimationMode.InAnimationMode());
-            if (GUILayout.Button(activity.previewSetup ? "Generate\npreview" : "Setup\npreview", GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true)))
-            {
-                if (activity.previewSetup)
+                if (clipIsInDict)
                 {
-                    new CgeActivityPreviewInternal(Repaint, activity, _animationClipToTextureDict, PictureWidth, PictureHeight, activity.editorArbitraryAnimations).Process(CgeActivityPreviewInternal.ProcessMode.CalculateMissing, element);
+                    GUILayout.Box(_animationClipToTextureDict[element], GUILayout.ExpandHeight(true), GUILayout.ExpandWidth(true));
+                    InvisibleRankPreservingButton();
                 }
                 else
                 {
-                    _editorMode = EditorMode.OtherOptions;
+                    InvisibleRankPreservingBox();
+                    EditorGUI.BeginDisabledGroup(AnimationMode.InAnimationMode());
+                    if (GUILayout.Button(activity.previewSetup ? "Generate\npreview" : "Setup\npreview", GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true)))
+                    {
+                        if (activity.previewSetup)
+                        {
+                            new CgeActivityPreviewInternal(Repaint, activity, _animationClipToTextureDict, PictureWidth, PictureHeight, activity.editorArbitraryAnimations).Process(CgeActivityPreviewInternal.ProcessMode.CalculateMissing, element);
+                        }
+                        else
+                        {
+                            _editorMode = EditorMode.OtherOptions;
+                        }
+                    }
+                    EditorGUI.EndDisabledGroup();
                 }
             }
-            EditorGUI.EndDisabledGroup();
-            EditorGUILayout.EndFadeGroup();
+            else
+            {
+                InvisibleRankPreservingBox();
+                InvisibleRankPreservingButton();
+            }
+        }
+
+        private static void InvisibleRankPreservingBox()
+        {
+            GUILayout.Box("", GUIStyle.none, GUILayout.Width(0), GUILayout.Height(0));
+        }
+
+        private static void InvisibleRankPreservingButton()
+        {
+            GUILayout.Button("", GUIStyle.none, GUILayout.Width(0), GUILayout.Height(0));
         }
 
         private void DoAutoSetupPreview()
         {
             _setupResult = new AutoSetupPreview(activity).AutoSetup();
         }
-    }
-
-    internal readonly struct MergePair
-    {
-        public MergePair(string left, string right)
-        {
-            Left = left;
-            Right = right;
-        }
-
-        public string Left { get; }
-        public string Right { get; }
     }
 }
---
+++ Assets/Hai/ComboGesture/Scripts/Editor/EditorUI/CgeActivityEditorDriver.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+
+namespace Hai.ComboGesture.Scripts.Editor.EditorUI
+{
+    public class CgeActivityEditorDriver
+    {
+        private static readonly Dictionary<string, string> Translations = new Dictionary<string, string>
+        {
+            {"anim00", "No gesture"},
+            {"anim01", "FIST"},
+            {"anim02", "OPEN"},
+            {"anim03", "POINT"},
+            {"anim04", "PEACE"},
+            {"anim05", "ROCKNROLL"},
+            {"anim06", "GUN"},
+            {"anim07", "THUMBSUP"},
+            {"anim11", "FIST x2, L+R trigger"},
+            {"anim11_L", "FIST x2, LEFT trigger"},
+            {"anim11_R", "FIST x2, RIGHT trigger"},
+            {"anim12", "OPEN + FIST"},
+            {"anim13", "POINT + FIST"},
+            {"anim14", "PEACE + FIST"},
+            {"anim15", "ROCKNROLL + FIST"},
+            {"anim16", "GUN + FIST"},
+            {"anim17", "THUMBSUP + FIST"},
+            {"anim22", "OPEN x2"},
+            {"anim23", "OPEN + POINT"},
+            {"anim24", "OPEN + PEACE"},
+            {"anim25", "OPEN + ROCKNROLL"},
+            {"anim26", "OPEN + GUN"},
+            {"anim27", "OPEN + THUMBSUP"},
+            {"anim33", "POINT x2"},
+            {"anim34", "POINT + PEACE"},
+            {"anim35", "POINT + ROCKNROLL"},
+            {"anim36", "POINT + GUN"},
+            {"anim37", "POINT + THUMBSUP"},
+            {"anim44", "PEACE x2"},
+            {"anim45", "PEACE + ROCKNROLL"},
+            {"anim46", "PEACE + GUN"},
+            {"anim47", "PEACE + THUMBSUP"},
+            {"anim55", "ROCKNROLL x2"},
+            {"anim56", "ROCKNROLL + GUN"},
+            {"anim57", "ROCKNROLL + THUMBSUP"},
+            {"anim66", "GUN x2"},
+            {"anim67", "GUN + THUMBSUP"},
+            {"anim77", "THUMBSUP x2"}
+        };
+
+        private static readonly Dictionary<string, MergePair> ParameterToMerge = new Dictionary<string, MergePair>
+        {
+            {"anim12", new MergePair("anim01", "anim02")},
+            {"anim13", new MergePair("anim01", "anim03")},
+            {"anim14", new MergePair("anim01", "anim04")},
+            {"anim15", new MergePair("anim01", "anim05")},
+            {"anim16", new MergePair("anim01", "anim06")},
+            {"anim17", new MergePair("anim01", "anim07")},
+            {"anim23", new MergePair("anim02", "anim03")},
+            {"anim24", new MergePair("anim02", "anim04")},
+            {"anim25", new MergePair("anim02", "anim05")},
+            {"anim26", new MergePair("anim02", "anim06")},
+            {"anim27", new MergePair("anim02", "anim07")},
+            {"anim34", new MergePair("anim03", "anim04")},
+            {"anim35", new MergePair("anim03", "anim05")},
+            {"anim36", new MergePair("anim03", "anim06")},
+            {"anim37", new MergePair("anim03", "anim07")},
+            {"anim45", new MergePair("anim04", "anim05")},
+            {"anim46", new MergePair("anim04", "anim06")},
+            {"anim47", new MergePair("anim04", "anim07")},
+            {"anim56", new MergePair("anim05", "anim06")},
+            {"anim57", new MergePair("anim05", "anim07")},
+            {"anim67", new MergePair("anim06", "anim07")}
+        };
+        private static readonly Dictionary<string, string> AnimationToCopy = new Dictionary<string, string>
+        {
+            {"anim11", "anim01"},
+            {"anim12", "anim02"},
+            {"anim13", "anim03"},
+            {"anim14", "anim04"},
+            {"anim15", "anim05"},
+            {"anim16", "anim06"},
+            {"anim17", "anim07"},
+            {"anim22", "anim02"},
+            {"anim33", "anim03"},
+            {"anim44", "anim04"},
+            {"anim55", "anim05"},
+            {"anim66", "anim06"},
+            {"anim77", "anim07"}
+        };
+
+        public bool IsSymmetrical(string propertyPath)
+        {
+            return Translations[propertyPath].Contains("x2");
+        }
+
+        public string ShortTranslation(string propertyPath)
+        {
+            return Translations[propertyPath];
+        }
+
+        public bool IsAPropertyThatCanBeCombined(string propertyPath)
+        {
+            return ParameterToMerge.ContainsKey(propertyPath);
+        }
+
+        public bool AreCombinationSourcesDefinedAndCompatible(SerializedObject serializedObject, string propertyPath)
+        {
+            if (!IsAPropertyThatCanBeCombined(propertyPath))
+            {
+                return false;
+            }
+
+            var mergePair = ParameterToMerge[propertyPath];
+            var left = serializedObject.FindProperty(mergePair.Left).objectReferenceValue;
+            var right = serializedObject.FindProperty(mergePair.Right).objectReferenceValue;
+
+            return left != null && right != null && left != right;
+        }
+
+        public MergePair ProvideCombinationPropertySources(string propertyPath)
+        {
+            if (!IsAPropertyThatCanBeCombined(propertyPath))
+            {
+                throw new ArgumentException();
+            }
+
+            return ParameterToMerge[propertyPath];
+        }
+
+        public bool IsAutoSettable(string propertyPath)
+        {
+            return AnimationToCopy.ContainsKey(propertyPath);
+        }
+
+        public string GetAutoSettableSource(string propertyPath)
+        {
+            return AnimationToCopy[propertyPath];
+        }
+    }
+
+    public readonly struct MergePair
+    {
+        public MergePair(string left, string right)
+        {
+            Left = left;
+            Right = right;
+        }
+
+        internal string Left { get; }
+        internal string Right { get; }
+    }
+}
